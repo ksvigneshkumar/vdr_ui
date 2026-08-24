e17135c2c40bfd76b4d2a3920172bdaec9bb7417
Anusiya-2005 <anusiyaselvaraj2005@gmail.com>
2026-08-24 11:04:41 +0530

    FIX:mobile responsiveness(Q&A)

--- app/qa/layout.jsx
+++ app/qa/layout.jsx
@@ -2,12 +2,12 @@ import MainSidebar from "@/components/MainSidebar";
 
 export default function QALayout({ children }) {
   return (
-    <div className="flex h-screen overflow-hidden bg-[#FAFBFD]">
+    <div className="flex flex-col md:flex-row w-full h-screen overflow-hidden bg-[#FAFBFD] font-sans pt-16 md:pt-0">
       {/* Main Global Sidebar */}
       <MainSidebar />
 
       {/* Main Content Area (which contains the QA secondary sidebar & table) */}
-      <div className="flex-1 flex flex-col min-w-0 overflow-y-auto">
+      <div className="flex-1 flex flex-col min-w-0 h-full overflow-hidden relative">
         {children}
       </div>
     </div>
